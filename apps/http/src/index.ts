4b8c57d977d29355051cb9013f540eb5b6b94182
Gaurav Kumar <gaurav989267@gmail.com>
2025-03-11 21:29:17 +0530

    feat(common): add TypeScript configuration and user schema; update backend-common and http package dependencies

--- apps/http/src/index.ts
+++ apps/http/src/index.ts
@@ -1,6 +1,18 @@
 import express, { Application } from 'express'
 
+// import jwt from 'jsonwebtokenon'
+
 const app:Application = express();
 
+app.post("/signup", (req, res) => {
+
+})
+
+
+app.post("/sigin", (req, res) => {});
+
+
+
+app.post("/room", (req, res) => {});
 
 app.listen(8001)
--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children} 58
+        {children} 
       </body>
     </html>
   );
