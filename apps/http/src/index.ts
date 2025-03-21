ff798800e18c46819966558ba5ceff5b7b476334
Gaurav Kumar <gaurav989267@gmail.com>
2025-03-22 00:14:44 +0530

    feat(validator): create validator package with CreateUserSchema; update HTTP app to use new schema

--- apps/http/src/index.ts
+++ apps/http/src/index.ts
@@ -4,11 +4,24 @@ import express, { Application, Request, Response } from "express";
 
 import { prismaClient } from "@repo/db/client";
 
-import { CreateUserSchema } from "@repo/common/types";
+
+import {CreateUserSchema} from '@repo/validator/types'
 
 const app: Application = express();
 
-app.post("/signup", (req, res) => {});
+app.post("/signup", (req, res) => {
+    const data = CreateUserSchema.safeParse(req.body);
+
+
+    if (!data.success) {
+        res.status(400).json({message:"Invalid Input"});
+        return;
+    }
+
+
+    res.json(data.data);
+
+});
 
 app.post("/sigin", (req, res) => {});
 
