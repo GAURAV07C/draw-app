168464a278ea24726e245689d732157fba32ba43
Gaurav Kumar <gaurav989267@gmail.com>
2025-03-22 20:59:01 +0530

    feat(ws): implement JWT authentication for WebSocket connections and add jsonwebtoken dependency

--- apps/ws/src/index.ts
+++ apps/ws/src/index.ts
@@ -1,9 +1,65 @@
 import { WebSocket, WebSocketServer } from "ws";
+import { JWT_SECRET } from "@repo/backend-common/config";
+import jwt from "jsonwebtoken";
 
 const wss: WebSocketServer = new WebSocketServer({ port: 8080 });
 
-wss.on("connection", (ws: WebSocket) => {
+interface User {
+  ws: WebSocket;
+  userId: string;
+  room: string[];
+}
+
+const users: User[] = [];
+
+function cheakUser(token: string): string | null {
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET);
+    if (typeof decoded === "string") {
+      return null;
+    }
+
+    if (!decoded || !decoded.userId) {
+      return null;
+    }
+    return decoded.userId;
+  } catch (error) {
+    return null;
+  }
+}
+
+wss.on("connection", (ws: WebSocket, request) => {
+  const url = request.url;
+
+  if (!url) {
+    return;
+  }
+
+  const queryParams = new URLSearchParams(url.split("?")[1]);
+
+  const token = queryParams.get("token");
+
+  const userId = token ? cheakUser(token) : null;
+
+  if (userId == null) {
+    ws.close();
+
+    return null;
+  }
+
+  users.push({ ws, userId, room: [] });
+
   ws.on("message", (data) => {
-    ws.send("something");
+    const pasrseData = JSON.parse(data as unknown as string);
+
+    if(pasrseData.type === "join_room") {
+      
+
+      const user = users.find((user) => user.ws === ws);
+      user?.room.push(pasrseData.room);
+      
+      
+    }
+
   });
 });
